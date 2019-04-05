f9a859e09e5332e546d4ef879c6ae690e2eda975
Chong <chongnguyen0128@mail.ru>
2019-04-05 23:43:36 +0300

    done(web)

--- web/main.py
+++ web/main.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
 app.secret_key = 'development_key'
 
 
+class Form(FlaskForm):
+    file = FileField("", validators=[DataRequired()])
+    submit = SubmitField('Classify')
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    pass
+    form = Form()
+    if request.method == "POST":
+        file = form.file.data
+        name = file.filename
+        print(name)
+        file.save(f"static/save/{name}")
+    return render_template("block.html", form=form)
 
 
 if __name__ == '__main__':
